d6963ce0fe99f5aa173a834931906e6e08d8f081
AymanNasser <aymannasr43@gmail.com>
2020-11-05 00:21:21 +0200

    Finish Lesson4 Part-2

--- Part2 - Camera /Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/descriptor_matching.cpp
+++ Part2 - Camera /Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/descriptor_matching.cpp
@@ -7,6 +7,19 @@
 
 #include "structIO.hpp"
 
+#define LOAD_LARGE_DATASET 0
+#define LOAD_BRISK_DATASET 0
+
+namespace const_hyperparams
+{
+    std::string MATCHER_BF = "MAT_BF";
+    std::string MATCHER_FLANN = "MAT_FLANN";
+    std::string DESC_BINARY = "DES_BIN";
+    std::string DESC_NOT_BINARY = "DES_NBIN";
+    std::string SELECT_NN = "SEL_NN";
+    std::string SELECT_KNN = "SEL_KNN";
+}
+
 using namespace std;
 
 void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> &kPtsSource, vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
@@ -22,35 +35,49 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
 
         int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
         matcher = cv::BFMatcher::create(normType, crossCheck);
-        cout << "BF matching cross-check=" << crossCheck;
+        cout << "BF matching cross-check=" << crossCheck << endl;
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
         if (descSource.type() != CV_32F)
-        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+        { 
+            // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
             descSource.convertTo(descSource, CV_32F);
             descRef.convertTo(descRef, CV_32F);
         }
 
-        //... TODO : implement FLANN matching
-        cout << "FLANN matching";
+        matcher = cv::FlannBasedMatcher::create();
+        cout << "FLANN matching" << endl;
     }
 
     // perform matching task
     if (selectorType.compare("SEL_NN") == 0)
-    { // nearest neighbor (best match)
-
+    { 
+        // nearest neighbor (best match)
         double t = (double)cv::getTickCount();
-        matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
+        matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor from a query set (descSource)
         t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
         cout << " (NN) with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
     }
     else if (selectorType.compare("SEL_KNN") == 0)
-    { // k nearest neighbors (k=2)
+    { 
+        // k nearest neighbors (k=2), finds the k best matches for each descriptor from a query set.
+        int k = 2;
+        float DESC_distanceThreshold = 0.8;
+        std::vector<std::vector<cv::DMatch>> knnMatches;
+        double t = (double)cv::getTickCount();
+        matcher->knnMatch(descSource, descRef, knnMatches, k);
+        t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        cout << " (KNN) with n=" << knnMatches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
 
-        // TODO : implement k-nearest-neighbor matching
+        // Filtering matches using descriptor distance ratio test
+        for(auto it = knnMatches.begin(); it!= knnMatches.end(); ++it){
 
-        // TODO : filter matches using descriptor distance ratio test
+            double distanceRatio = (*it)[0].distance / (*it)[1].distance;
+            if(distanceRatio < DESC_distanceThreshold)
+                matches.push_back((*it)[0]);
+        }
+        cout << "Keypoints removed = " << knnMatches.size() - matches.size() << endl;
     }
 
     // visualize results
@@ -59,7 +86,7 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
                     matchImg, cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
     string windowName = "Matching keypoints between two camera images (best 50)";
-    cv::namedWindow(windowName, 7);
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     cv::imshow(windowName, matchImg);
     cv::waitKey(0);
 }
@@ -69,17 +96,42 @@ int main()
     cv::Mat imgSource = cv::imread("../images/img1gray.png");
     cv::Mat imgRef = cv::imread("../images/img2gray.png");
 
-    vector<cv::KeyPoint> kptsSource, kptsRef; 
-    readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
-    readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
 
+    vector<cv::KeyPoint> kptsSource, kptsRef; 
     cv::Mat descSource, descRef; 
-    readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
-    readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
+    
+    if(LOAD_BRISK_DATASET)
+    {
+        if(LOAD_LARGE_DATASET)
+        {
+            readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
+            readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
+
+            readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
+            readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
+        }
+        else
+        {
+            readKeypoints("../dat/C35A5_KptsSource_BRISK_small.dat", kptsSource);
+            readKeypoints("../dat/C35A5_KptsRef_BRISK_small.dat", kptsRef);
+
+            readDescriptors("../dat/C35A5_DescSource_BRISK_small.dat", descSource);
+            readDescriptors("../dat/C35A5_DescRef_BRISK_small.dat", descRef);
+        }
+    }
+    else
+    {
+        readKeypoints("../dat/C35A5_KptsSource_SIFT.dat", kptsSource);
+        readKeypoints("../dat/C35A5_KptsRef_SIFT.dat", kptsRef);
+
+        readDescriptors("../dat/C35A5_DescSource_SIFT.dat", descSource);
+        readDescriptors("../dat/C35A5_DescRef_SIFT.dat", descRef);   
+    }    
 
     vector<cv::DMatch> matches;
-    string matcherType = "MAT_BF"; 
-    string descriptorType = "DES_BINARY"; 
-    string selectorType = "SEL_NN"; 
+    string matcherType = const_hyperparams::MATCHER_BF; 
+    string descriptorType = const_hyperparams::DESC_BINARY; 
+    string selectorType = const_hyperparams::SELECT_KNN;
+
     matchDescriptors(imgSource, imgRef, kptsSource, kptsRef, descSource, descRef, matches, descriptorType, matcherType, selectorType);
 }
