8ad4e1bed4af4e0d4573a68f6bf0fba9226a80ee
AymanNasser <aymannasr43@gmail.com>
2020-12-26 06:46:43 +0200

    Finished lesson-3

---
+++ Part4 - Kalman Filters/Lesson-3/jacobian_calc/jac.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include "../eigen/Eigen/Dense"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+
+MatrixXd CalculateJacobian(const VectorXd& x_state);
+
+int main() {
+  /**
+   * Compute the Jacobian Matrix
+   */
+
+  // predicted state example
+  // px = 1, py = 2, vx = 0.2, vy = 0.4
+  VectorXd x_predicted(4);
+  x_predicted << 1, 2, 0.2, 0.4;
+
+  MatrixXd Hj = CalculateJacobian(x_predicted);
+
+  cout << "Hj:" << endl << Hj << endl;
+
+  return 0;
+}
+
+MatrixXd CalculateJacobian(const VectorXd& x_state) {
+
+  MatrixXd Hj(3,4);
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // TODO: YOUR CODE HERE 
+
+  // compute the Jacobian matrix
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+  
+  // check division by zero
+  if(px ==0 || py ==0)
+    throw std::overflow_error("Division by zero");
+
+  
+  Hj << (px/c2), (py/c2), 0, 0,
+        -(py/c1), (px/c1), 0, 0,
+        py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
+}
